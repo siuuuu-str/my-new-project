54aca04f50995b6805ebdf0b60ca1d54c074ec39
siuuuu-str <banditagod0@gmail.com>
2026-09-10 13:15:00 +0900

    Revert "Добавление проекта змейки"

--- faker_example.py
+++
@@ -1,5 +0,0 @@
-from faker import Faker
-fake = Faker()
-
-print("Случайно имя:", fake.name())
-print("Случайно адрес:", fake.address())
--- main.py
+++
@@ -1,5 +0,0 @@
-user_score = 10
-y = 5
-print(user_score + y)
-print("user_score равен:", user_score)
-print("y равен:", y)
